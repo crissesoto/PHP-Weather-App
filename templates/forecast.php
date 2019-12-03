85ac9257a480c7f33857f1afd5a6888d57294c5e
crissesoto <soto.crisse@hotmail.com>
2019-12-03 22:09:18 +0100

    added curl

--- templates/forecast.php
+++ templates/forecast.php
@@ -20,3 +20,31 @@ const getCity = async cityname => {
 
   return data[0];
 };
+
+// php 
+
+<?php 
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://jsonplaceholder.typicode.com/todos",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => array(
+    "cache-control: no-cache"
+  ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+$response = json_decode($response, true); //because of true, it's in an array
+
+ChromePhp::log($response);
+
+?>
