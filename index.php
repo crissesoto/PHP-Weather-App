4a15d91a50e8d1ce3841d136868f10b889af0ec6
crissesoto <soto.crisse@hotmail.com>
2019-12-04 09:54:16 +0100

    functions in forecast.php

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php include "templates/forecast.php" ?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
--- templates/forecast.php
+++ templates/forecast.php
@@ -1,42 +1,28 @@
-const key = "L6gtFl47bvHJUf6TJHb6jWgAwQ3z7MBK";
 
-const getCurrentWeather = async cityKey => {
-  const baseURL = "http://dataservice.accuweather.com/currentconditions/v1/";
-  const query = `${cityKey}?apikey=${key}`;
-
-  const response = await fetch(baseURL + query);
-  const data = await response.json();
-
-  return data[0];
-};
-
-const getCity = async cityname => {
-  const baseURL =
-    "http://dataservice.accuweather.com/locations/v1/cities/search";
-  const query = `?apikey=${key}&q=${cityname}`;
-
-  const response = await fetch(baseURL + query);
-  const data = await response.json();
-
-  return data[0];
-};
-
-// php 
+<?php include "ChromePhp.php" ?>
 
 <?php 
 
-$curl = curl_init();
 
-curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://jsonplaceholder.typicode.com/todos",
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_TIMEOUT => 30,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "GET",
-  CURLOPT_HTTPHEADER => array(
+// Get city key for current weather
+
+function getCurrentWeather($cityKey){
+  $key = "L6gtFl47bvHJUf6TJHb6jWgAwQ3z7MBK";
+  $baseURL = "http://dataservice.accuweather.com/currentconditions/v1/$cityKey?apikey=$key";
+  #$query = "$cityKey?apikey=$key";
+
+  $curl = curl_init();
+
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => $baseURL,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "GET",
+    CURLOPT_HTTPHEADER => array(
     "cache-control: no-cache"
-  ),
-));
+    ),
+  ));
 
 $response = curl_exec($curl);
 $err = curl_error($curl);
@@ -44,7 +30,48 @@ $err = curl_error($curl);
 curl_close($curl);
 
 $response = json_decode($response, true); //because of true, it's in an array
+$data = $response[0];
 
-ChromePhp::log($response);
+//return $data;
+ChromePhp::log($data);
 
+}
+
+
+// Get city name for weather info
+
+function getCity($cityname){
+  $key = "L6gtFl47bvHJUf6TJHb6jWgAwQ3z7MBK";
+  //$cityname = "london";
+  $baseURL = "http://dataservice.accuweather.com/locations/v1/cities/search?apikey=$key&q=$cityname";
+  #$query = "?apikey=$key&q=$cityName;
+
+  $curl = curl_init();
+
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => $baseURL,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "GET",
+    CURLOPT_HTTPHEADER => array(
+    "cache-control: no-cache"
+    ),
+  ));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+$response = json_decode($response, true); //because of true, it's in an array
+$data = $response[0];
+
+//return $data;
+ChromePhp::log($data);
+
+};
+
+getCity("london");
+getCurrentWeather("328328");
 ?>
